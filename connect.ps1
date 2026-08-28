36baa2afb7d95e01c127492f6b3f2b041638ea50
Williamjensen <108738188+Williamjensen17@users.noreply.github.com>
2026-08-28 11:06:18 +0200

    Add server monitoring script in connect.ps1

---
+++ connect.ps1
@@ -0,0 +1,78 @@
+# import da fuggin list mate
+$RAMNeeded = 8          #RAM ammount in GB 
+$DiskNeeded = 20        #Disk ammount in GB
+
+$data = Import-Csv .\Serverlist.csv     #Csv location
+
+
+function GetData {
+    param ($Server)
+    $Hostname = Invoke-Command $Server -ScriptBlock{Hostname}
+    $OS = Invoke-Command $Server -ScriptBlock{(Get-CimInstance Win32_OperatingSystem).Caption}
+    $Processors = Invoke-Command $Server -ScriptBlock{(Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors}
+    $RAMCount = Invoke-Command $Server -ScriptBlock{(Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb}
+    $BootTime = Invoke-Command $Server -ScriptBlock{(Get-CimInstance Win32_OperatingSystem).LastBootUpTime}    
+
+    Write-host Hostname: $Hostname
+    Write-host OS: $OS
+    Write-host Cores: $Processors
+    Write-host RAM: $RAMCount GB
+    Write-host IP: $Server
+    Write-host Last Boot: $BootTime
+    Write-host
+    Write-host
+    Invoke-Command $Server -ScriptBlock{gwmi win32_logicaldisk | Format-Table DeviceId, MediaType, @{n="Size";e={[math]::Round($_.Size/1GB,2)}},@{n="FreeSpace";e={[math]::Round($_.FreeSpace/1GB,2)}}}
+    
+}
+
+
+function Control {
+    param ($Server)
+
+    $Online = test-connection $Server
+    if (!($Online)) {Write-host Connection status: OFFLINE -foregroundcolor red}
+    else{Write-host Connection status: Up -foregroundcolor green}
+
+    $DiskSpace = Invoke-Command $Server -ScriptBlock{"{0:N2}" -f ((Get-CimInstance Win32_LogicalDisk -Filter "DeviceID='C:'").FreeSpace / 1GB)}
+    if ($DiskSpace -lt $DiskNeeded)  {Write-host Disk is less than $DiskNeeded GB $DiskSpace -foregroundcolor red}
+    else {Write-host more than $DiskNeeded GB left $DiskSpace -foregroundcolor green}
+
+    #enough ram
+    $RAMCount = Invoke-Command $Server -ScriptBlock{(Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb}
+    if ($RAMCount -lt $RAMNeeded) {Write-host Ram is less than $RAMNeeded GB $RAMCount -foregroundcolor red}
+    else {Write-host more than $RAMNeeded GB left $RAMCount -foregroundcolor green}
+
+    $Spooler = Get-Service -name Spooler | Select-Object Status
+    if ($Spooler){Write-host Spooler Status: $Spooler.status}
+    else {Write-host Spooler Status: $Spooler.status}
+
+
+    #Status
+    if (!($Online)){Write-host Server Status: OFFLINE -foregroundcolor yellow; return}
+    
+
+}
+
+foreach ($Server in $data) 
+{
+    try {
+
+        $header = @"
+========================================
+            $($Server.IP)
+========================================
+"@
+
+
+        Write-host $header
+        if (!(test-connection $Server.IP)) {Write-host Server Status: OFFLINE -foregroundcolor yellow}
+        else {        
+            GetData -Server $Server.IP
+            Control -Server $Server.IP
+        }
+
+    }
+    catch {
+        $_;
+    } 
+}
